92a410c932b784c012df64b7c976aa5a5fefd8ad
Ihor Poliakov <piv2003@gmail.com>
2023-08-08 22:57:33 +0200

    fixing errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
-import { App } from 'components/App';
+import { store } from './Redux/store';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
