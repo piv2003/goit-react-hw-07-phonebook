3740a682177ff0618b36f5cce5b56ea5b00f7e7f
Ihor Poliakov <piv2003@gmail.com>
2023-08-09 23:25:04 +0200

    adding initialState

--- src/Redux/contactsSlice.js
+++ src/Redux/contactsSlice.js
@@ -1,2 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './operations';
+
+const initialState = {
+  contacts: [],
+  isLoading: false,
+  error: null,
+};
