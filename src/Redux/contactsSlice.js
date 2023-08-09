4a29cca63b9171faf8b7b63f98bf428205965cb0
Ihor Poliakov <piv2003@gmail.com>
2023-08-09 23:28:17 +0200

    adding handleRejected

--- src/Redux/contactsSlice.js
+++ src/Redux/contactsSlice.js
@@ -11,3 +11,8 @@ const handlePending = state => {
   state.isLoading = true;
   state.error = null;
 };
+
+const handleRejected = (state, action) => {
+  state.isLoading = true;
+  state.error = action.payload;
+};
