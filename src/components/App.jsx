9177f7b32063cddd8a34b31777a9c90281066b55
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 15:30:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,3 +11,26 @@ import Form from './Form/Form';
 import ContactsList from './ContactsList/ContactsList';
 import { AppBox } from './App.styled';
 import { addContact, fetchContacts } from '../Redux/operations';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  return (
+    <AppBox>
+      <ToastContainer autoClose={3000} position="top-center" />
+      <h1>Phonebook</h1>
+      <Form onSubmit={onSubmit} />
+
+      <h2>Contacts</h2>
+
+      {isLoading && !error && <b>Request in progress...</b>}
+
+      {error && <p>Something went wrong..</p>}
+
+      <ContactsList />
+    </AppBox>
+  );
+}
