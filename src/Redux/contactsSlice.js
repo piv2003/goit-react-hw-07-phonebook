c49e814de54b361b511a3101db27e37c17e5a93d
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 22:33:33 +0200

    Update contactsSlice.js

--- src/Redux/contactsSlice.js
+++ src/Redux/contactsSlice.js
@@ -16,3 +16,41 @@ const handleRejected = (state, action) => {
   state.isLoading = false;
   state.error = action.payload;
 };
+
+const createAsyncSlice = (name, initialState) => {
+  return createSlice({
+    name,
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+      builder
+        .addCase(fetchContacts.pending, handlePending)
+        .addCase(fetchContacts.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.contacts = action.payload;
+        })
+        .addCase(fetchContacts.rejected, handleRejected)
+        .addCase(addContact.pending, handlePending)
+        .addCase(addContact.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.contacts.push(action.payload);
+        })
+        .addCase(addContact.rejected, handleRejected)
+        .addCase(deleteContact.pending, handlePending)
+        .addCase(deleteContact.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.error = null;
+          state.contacts = state.contacts.filter(
+            contact => contact.id !== action.payload.id
+          );
+        })
+        .addCase(deleteContact.rejected, handleRejected);
+    },
+  });
+};
+
+const contactsSlice = createAsyncSlice('contacts', initialState);
+
+export const contactsReducer = contactsSlice.reducer;
