5a0483294c6e51952490c35724299a207d8a1427
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 23:28:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
-import { App } from './components/App';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
