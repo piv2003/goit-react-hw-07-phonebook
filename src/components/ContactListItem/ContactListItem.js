c9fd3c82a698074fcc12a773e7978c5d24fbf355
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 23:30:04 +0200

    Update ContactListItem.js

--- src/components/ContactListItem/ContactListItem.js
+++ src/components/ContactListItem/ContactListItem.js
@@ -12,23 +12,8 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { RiDeleteBin2Line } from 'react-icons/ri';
 
-const ContactsListItem = ({ id, name, number, onDeleteContact }) => {
-  return (
-    <Item id={id}>
-      <Name>{name}</Name>
-      <Number>{number}</Number>
-      <button type="button" onClick={() => onDeleteContact(id)}>
-        Delete
-      </button>
-    </Item>
-  );
-};
-
 ContactsListItem.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+  contact: PropTypes.object.isRequired,
 };
 
 export default ContactsListItem;
