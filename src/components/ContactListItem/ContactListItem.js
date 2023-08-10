bfdee06ba20c46ddc23842664bf7257bcf5465d2
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 23:33:45 +0200

    Update ContactListItem.js

--- src/components/ContactListItem/ContactListItem.js
+++ src/components/ContactListItem/ContactListItem.js
@@ -12,7 +12,22 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { RiDeleteBin2Line } from 'react-icons/ri';
 
-const ContactsListItem = ({ contact }) => {};
+const ContactsListItem = ({ contact }) => {
+  const [contactId, setContactId] = useState(null);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+    setContactId(contact.id);
+    if (!error) {
+      toast.success(`Contact ${contact.name} successfully deleted`, {
+        position: 'top-right',
+      });
+    }
+  };
+};
 
 ContactsListItem.propTypes = {
   contact: PropTypes.object.isRequired,
