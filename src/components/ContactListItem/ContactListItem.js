675691e45afce10c1185570132403d3c5fa3c2b9
Ihor Poliakov <piv2003@gmail.com>
2023-08-10 23:27:41 +0200

    Update ContactListItem.js

--- src/components/ContactListItem/ContactListItem.js
+++ src/components/ContactListItem/ContactListItem.js
@@ -1,5 +1,16 @@
 import PropTypes from 'prop-types';
-import { Item, Name, Number } from './ContactListItem.styled';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import {
+  Button,
+  ContactInfo,
+  Name,
+  Number,
+  Spinner,
+  UserIcon,
+} from './ContactListItem.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { RiDeleteBin2Line } from 'react-icons/ri';
 
 const ContactsListItem = ({ id, name, number, onDeleteContact }) => {
   return (
