268b3a5ad1cc64bfb2fdcb76906f3b3baecc3f24
Ihor Poliakov <piv2003@gmail.com>
2023-08-11 16:26:49 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,4 +3,21 @@ import { LabelFilter, InputFilter } from './Filter.styled';
 import { onChangeFilter } from '../../Redux/filterSlice';
 import { selectFilter } from '../../Redux/selectors';
 
+const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
+
+  return (
+    <LabelFilter>
+      <span>Find contacts by name</span>
+      <InputFilter
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleInputChange}
+      />
+    </LabelFilter>
+  );
+};
+
 export default Filter;
