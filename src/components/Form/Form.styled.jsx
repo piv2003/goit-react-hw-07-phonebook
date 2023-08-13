e702315e7ff2d76a9d7bbc5431caa4e9886bd8d1
Ihor Poliakov <piv2003@gmail.com>
2023-08-13 20:40:54 +0200

    Update Form.styled.jsx

--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -1,29 +1,33 @@
-import styled from 'styled-components';
 import { Form, Field, Formik } from 'formik';
+import styled, { keyframes } from 'styled-components';
 
 export const FormikWrapper = styled(Formik)``;
 
-export const FormBox = styled.form`
+export const FormBox = styled(Form)`
   margin-left: auto;
   margin-right: auto;
-  padding: 15px;
-  background: white;
+  padding: 20px;
+  width: 260px;
+  background: #fff;
   border-radius: 30px;
-  box-shadow: rgba(0, 0, 0, 0.45) 0px 75px 75px,
-    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-    rgba(0, 0, 0, 0.17) 0px 12px 21px, rgba(0, 0, 0, 0.09) 0px -3px 2px;
+  box-shadow: rgba(0, 0, 0, 0.986), 5px 7px 10px,
+    rgba(0, 0, 0, 0.9) 3px -3px 10px, rgba(0, 0, 0, 0.9) 0px 4px 6px,
+    rgba(0, 0, 0, 0.9) 10px 3px 5px, rgba(0, 0, 0, 0.9) 10px -3px 2px;
 
   & label {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
     & span {
-      margin-bottom: 15px;
+      margin-bottom: 5px;
       font-weight: bold;
       color: #8005c7;
     }
   }
 `;
 
-export const InputName = styled.input`
-  display: block;
+export const InputName = styled(Field)`
   padding: 0 20px;
   margin-bottom: 10px;
   background: #e9eff6;
@@ -31,12 +35,21 @@ export const InputName = styled.input`
   border-width: 0;
   border-radius: 20px;
   font-family: 'Roboto', sans-serif;
-  font-style: normal;
   font-size: 18px;
 `;
 
-export const InputTel = styled(InputName)`
-  font-style: normal;
+export const InputTel = styled(InputName)``;
+
+const clickEffect = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.9);
+  }
+  100% {
+    transform: scale(1);
+  }
 `;
 
 export const Button = styled.button`
@@ -49,6 +62,7 @@ export const Button = styled.button`
   height: 40px;
   font-weight: 700;
   font-size: 17px;
+  text-shadow: 1px 1px #000;
   line-height: normal;
   margin: 20px;
   border: 2px solid pink;
@@ -57,4 +71,17 @@ export const Button = styled.button`
   box-shadow: rgba(0, 0, 0, 0.45) 0px 5px 10px, rgba(0, 0, 0, 0.12) 0px -3px 5px,
     rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 3px 5px,
     rgba(0, 0, 0, 0.09) 0px -3px 2px;
+  background: radial-gradient(circle, #f1c1bc, #c4ee08);
+  background-clip: padding-box;
+  transition: background-image 0.3s, color 0.3s, transform 0.3s;
+
+  &:hover {
+    background: radial-gradient(circle, #d3e680, #f1c1bc);
+    color: darkblue;
+    transform: scale(1.1);
+  }
+
+  &:active {
+    animation: ${clickEffect} 1.2s;
+  }
 `;
