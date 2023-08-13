18205f29ace396217781a0b49b13f6731b583b94
Ihor Poliakov <piv2003@gmail.com>
2023-08-13 20:37:43 +0200

    Update ContactsList.styled.jsx

--- src/components/ContactsList/ContactsList.styled.jsx
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -31,3 +31,25 @@ export const Item = styled.li`
     rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
     rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
 `;
+
+export const Empty = styled.p`
+  font-weight: bold;
+  @-webkit-keyframes pulsate {
+    50% {
+      color: #fff;
+      text-shadow: 0 -1px rgba(0, 0, 0, 0.3), 0 0 5px #ffd, 0 0 8px #fff;
+    }
+  }
+  @keyframes pulsate {
+    50% {
+      color: #d60bf1;
+      text-shadow: 0 -1px rgba(0, 0, 0, 0.3), 0 0 5px #ffd, 0 0 8px #fff;
+    }
+  }
+  color: rgb(245, 245, 245);
+  text-shadow: 0 -1px rgba(0, 0, 0, 0.1);
+  background: radial-gradient(#c801f0, #fdfa4c);
+  border-radius: 7px;
+  -webkit-animation: pulsate 2.2s linear infinite;
+  animation: pulsate 2.2s linear infinite;
+`;
